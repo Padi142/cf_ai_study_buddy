f79d82feeff486a0d61a08c4be0d176f3e20f86a
Padias <matyas.krejza@seznam.cz>
2025-12-23 21:59:01 +0100

    fix build

--- eslint.config.js
+++ eslint.config.js
@@ -54,6 +54,7 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-return": "warn",
       "@typescript-eslint/prefer-nullish-coalescing": "warn",
       "import/no-anonymous-default-export": "warn",
+      "@typescript-eslint/no-floating-promises": "warn",
       "prefer-const": "warn",
     },
   },
